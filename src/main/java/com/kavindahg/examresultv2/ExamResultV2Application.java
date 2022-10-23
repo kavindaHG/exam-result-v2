189b2f4e3967c7faa5919724ddebe7f43654c2b7
kavindaHG <90039120+kavindaHG@users.noreply.github.com>
2022-10-23 11:07:37 +0530

    initial commit

---
+++ src/main/java/com/kavindahg/examresultv2/ExamResultV2Application.java
@@ -0,0 +1,13 @@
+package com.kavindahg.examresultv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamResultV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamResultV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/kavindahg/examresultv2/ExamResultV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kavindahg.examresultv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamResultV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
